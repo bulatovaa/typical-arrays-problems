40fa41eba03384df6d831c01e3c82dd97747344b
Ekaterina Bulatova <katerinavitalevna@ya.ru>
2020-09-29 12:32:13 +0400

    feat: solve the task

--- src/index.js
+++ src/index.js
@@ -1,12 +1,28 @@
 
 exports.min = function min (array) {
-  return 0;
+  if (!Array.isArray(array) || array.length === 0) {
+    return 0;
+  }
+  const arrSort = array.sort((a, b) => a - b);
+  return arrSort[0];
 }
 
 exports.max = function max (array) {
-  return 0;
+  if (!Array.isArray(array) || array.length === 0) {
+    return 0;
+  }
+  const arrSort = array.sort((a, b) => b - a);
+  return arrSort[0];
 }
 
 exports.avg = function avg (array) {
-  return 0;
+  if (!Array.isArray(array) || array.length === 0) {
+    return 0;
+  }
+  const arrSort = array.sort((a, b) => a - b);
+  let sum = 0;
+  for (let i = 0; i < arrSort.length; i++) {
+    sum += arrSort[i];
+  }
+  return sum / array.length;
 }
